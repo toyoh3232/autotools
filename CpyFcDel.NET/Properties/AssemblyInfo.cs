8e8138b3f02ed3ec454f553fced79b58adfe9e79
Toyoharu Han <toyoh3232d@protonmail.com>
2020-06-16 18:05:04 +0900

    0616 commandline fix

--- CpyFcDel.NET/Properties/AssemblyInfo.cs
+++ CpyFcDel.NET/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CpyFcDel.NET")]
 [assembly: AssemblyDescription(".NET version of CpyFcDel")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tohasoft")]
+[assembly: AssemblyCompany("Toyoharu Han")]
 [assembly: AssemblyProduct("CpyFcDel.NET")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
